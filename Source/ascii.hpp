ea2b4220404d041af9fc975ede20532c5760b307
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2023-11-13 00:19:55 -0300

    Update ascii.hpp

--- Source/ascii.hpp
+++ Source/ascii.hpp
@@ -6,10 +6,10 @@ namespace ASCII
 {
 	char convertToAscii(int r, int g, int b)
 	{
-        int intensityGray = (int)(0.299 * r + 0.587 * g + 0.114 * b);
+        	int intensityGray = (int)(0.299 * r + 0.587 * g + 0.114 * b);
 
-        char charList[] = { ' ', '.', ':', '-', '=', '+', '*', '#', '%', '@' };
+        	char charList[] = { ' ', '.', ':', '-', '=', '+', '*', '#', '%', '@' };
 		
-        return charList[intensityGray * sizeof(charList) / 256];
+        	return charList[intensityGray * sizeof(charList) / 256];
 	}
 }
